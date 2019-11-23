a8b4910bb418f576c7734cd98052818fcf7d82e2
Cristian Mansilla <cristian-mansilla@live.com.ar>
2019-11-23 17:24:00 -0300

    añadiendo campos a tabla users

---
+++ database/migrations/2019_11_23_192753_edit_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->renameColumn('name','nombre');
+          $table->string('apellido');
+          $table->string('usuario');
+          $table->string('img_perfil');
+          $table->unsignedBigInteger('ciudad_id');
+          $table->foreign('ciudad_id')->references('id')->on('ciudades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
