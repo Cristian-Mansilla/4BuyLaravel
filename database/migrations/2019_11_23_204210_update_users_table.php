22800198c5bdfa7620dc09cb0525abf6c7ac245c
Cristian Mansilla <cristian-mansilla@live.com.ar>
2019-11-23 17:48:11 -0300

    añadiendo clave foranea ciudad a tabla users

---
+++ database/migrations/2019_11_23_204210_update_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->unsignedBigInteger('ciudad_id');
+          $table->foreign('ciudad_id')->references('id')->on('ciudades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->bropForeign('users_ciudad_id_foreign');
+        });
+    }
+}
