a3f36b42a39ecb783ba653eabc4c10be3bbd099e
Nicolas <nmorero00@gmail.com>
2019-11-26 22:09:25 -0300

    Sombras en index

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    var a = this.document.querySelectorAll('a');
+    for(var i = 0; i < a.length; i++){
+        a[i].style.textDecoration = 'none';
+    }
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,37 +7,37 @@
         <!-- Carousel -->
         <div id="carouselExampleIndicators" class="carousel slide col-12 p-0" data-ride="carousel" data-delay="3">
             <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
-            <div class="carousel-item active" id="caroussel1">
+                <div class="carousel-item active" id="caroussel1">
 
-            </div>
-            <div class="carousel-item" id="caroussel2">
+                </div>
+                <div class="carousel-item" id="caroussel2">
 
-            </div>
-            <div class="carousel-item" id="caroussel3">
+                </div>
+                <div class="carousel-item" id="caroussel3">
 
-            </div>
+                </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
             </a>
         </div>
 
-        <div class="container-fluid">
+        <div class="container-fluid ">
             <div class="container">
                 <!-- Bannerts Tarjetas -->
-                <div class="col-12 border-bottom justify-content-between d-flex mt-2 ">
-                    <img src="{{asset('/Imagenes/Banners/banner_tarjeta1.png')}}" height="100" width="500" alt="Banert Tarjetas 1" class="col-lg-6 col-md-12">
-                    <img src="{{asset('/Imagenes/Banners/banner_tarjeta2.png')}}" height="100" width="500" alt="Baner Tarjetas 2" class="col-lg-6 d-lg-block d-sm-none d-md-none d-none ">
+                <div class="col-12 justify-content-center d-flex mt-2 ">
+                    <img src="{{asset('/Imagenes/Banners/banner_tarjeta1.png')}}" height="100" width="500" alt="Banert Tarjetas 1" class="col-lg-6 col-md-12 sombra p-0 mr-4">
+                    <img src="{{asset('/Imagenes/Banners/banner_tarjeta2.png')}}" height="100" width="500" alt="Baner Tarjetas 2" class="col-lg-6 d-lg-block d-sm-none d-md-none d-none p-0 sombra">
                 </div>
                 <br><br><br>
                 <!-- Mas Vendidos -->
@@ -46,13 +46,13 @@
                                 <h2 class="col-12 border-bottom border-secondary">Lo mas vendido</h2>
 
                                 @foreach($prodMasVendidos as $producto)
-                                    <div class='zoom col-lg-3 col-md-12 mt-2 mb-5 p-3 text-center' style='height:300px;'>
+                                    <div class='zoom col-lg-3 col-md-12 mt-2 mb-5 text-center' style='height:300px;'>
                                         <a href='#top' class=' text-dark text-decoration-none'>
-                                            <div class='card' style='height:300px;'>
+                                            <div class='card sombra' style='height:300px;'>
                                                 <div class='card-img-top p-3'>
                                                     <img src=' {{$producto->ruta_imagen}} ' height='150' alt='...'>
                                                 </div>
-                                                <div class='card-body'>
+                                                <div class='card-body border-top'>
                                                     <h5 class='card-title'>{{$producto->titulo}}</h5>
                                                     <p class='card-text'>$ {{$producto->precio}}</p>
                                                 </div>
@@ -69,7 +69,7 @@
                                 @foreach($prodUltimaVisita as $producto)
                                     <div class='zoom col-lg-3 col-md-12 mt-2 mb-5 p-3 text-center' style='height:300px;'>
                                         <a href='#top' class=' text-dark text-decoration-none'>
-                                            <div class='card' style='height:300px;'>
+                                            <div class='card sombra' style='height:300px;'>
                                                 <div class='card-img-top p-3'>
                                                     <img src=' {{$producto->ruta_imagen}} ' height='150' alt='...'>
                                                 </div>
@@ -90,54 +90,54 @@
                         <h2 class="text-center col-12">CATEGORIAS MAS BUSCADAS</h2>
                         <br><br>
 
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=computadoras"><img  src=" {{ url('/Imagenes/Index/Categorias/Laptop.png') }} " class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Notebooks</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=computadoras"><img  src=" {{ url('/Imagenes/Index/Categorias/Laptop.png') }} " class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Notebooks</p>
+                            </div></a>
                         </div>
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias" >
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=celulares"><img  src="{{url('/Imagenes/Index/Categorias/Phone.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Celulares</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias" >
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=celulares"><img  src="{{url('/Imagenes/Index/Categorias/Phone.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Celulares</p>
+                            </div></a>
                         </div>
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=videojuegos"><img  src="{{url('/Imagenes/Index/Categorias/Juegos.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Videojuegos</p>
-                        </div></a>
+                            <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=videojuegos"><img  src="{{url('/Imagenes/Index/Categorias/Juegos.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Videojuegos</p>
+                            </div></a>
                         </div>
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=herramientas"><img  src="{{url('/Imagenes/Index/Categorias/Tools.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Herramientas</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark " href="categorias.php?categoria=herramientas"><img  src="{{url('/Imagenes/Index/Categorias/Tools.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Herramientas</p>
+                            </div></a>
                         </div>
 
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=hogar"><img  src="{{url('/Imagenes/Index/Categorias/House.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Hogar</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=hogar"><img  src="{{url('/Imagenes/Index/Categorias/House.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Hogar</p>
+                            </div></a>
                         </div>
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=autos"><img  src="{{url('/Imagenes/Index/Categorias/Car.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Autos</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=autos"><img  src="{{url('/Imagenes/Index/Categorias/Car.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Autos</p>
+                            </div></a>
                         </div>
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=colchonesSommiers"><img  src="{{ url('/Imagenes/Index/Categorias/Bed.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Colchones y Sommiers</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=colchonesSommiers"><img  src="{{ url('/Imagenes/Index/Categorias/Bed.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Colchones y Sommiers</p>
+                            </div></a>
                         </div>
-                        <div class="zoom card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
-                        <a class="text-decoration-none text-dark" href="categorias.php?categoria=televisor"><img  src="{{url('/Imagenes/Index/Categorias/TV.png')}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text text-center ">Televisores</p>
-                        </div></a>
+                        <div class=" card p-lg-4 p-5  col-lg-2 col-md-5 col-sm-6 backg-categorias">
+                            <a class="text-decoration-none text-dark" href="categorias.php?categoria=televisor"><img  src="{{url('/Imagenes/Index/Categorias/TV.png')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text text-center ">Televisores</p>
+                            </div></a>
                         </div>
 
                 </div>
